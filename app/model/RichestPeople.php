ff157d974696e12d62db0342cd12a6c0edd9a08c
Imran <333727@student.mboutrecht.nl>
2022-10-18 09:11:04 +0200

    Ik ben nu bezig met de model

---
+++ app/model/RichestPeople.php
@@ -0,0 +1,17 @@
+<?php
+
+class RichestPeople{
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new Database();
+    }
+
+
+    public function getRichestPeople()
+    {
+        $this->db->query("SELECT * FROM richestpeople ORDER BY Networth DESC;");
+        $result
+    }
+}

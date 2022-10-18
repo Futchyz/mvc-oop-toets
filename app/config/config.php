f3fa3691a1652f57410739d06b045cf7987ab16d
Imran <333727@student.mboutrecht.nl>
2022-10-18 09:02:15 +0200

    Ik ben klaar met de database

---
+++ app/config/config.php
@@ -0,0 +1,14 @@
+<?php
+    define('DB_HOST', 'localhost'); 
+    define('DB_NAME', 'richestpeople'); 
+    define('DB_USER', 'root');        
+    define('DB_PASS', '');           
+    
+    //APPROOT
+    define('APPROOT', dirname(dirname(__FILE__)));
+
+    //URLROOT (Dynamic links)
+    define('URLROOT', 'http://mvc-oop-toets.com');
+
+    //Sitename
+    define('SITENAME', 'MVC Toets');
